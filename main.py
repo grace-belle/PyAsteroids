2742c4781782602ac6722fa46342a4de54bb5546
grace-belle <millergb2000@gmail.com>
2026-01-30 22:16:03 -0600

    Adding more to the asteroids game

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
-
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
+    print("Screen width: 1280")
+    print("Screen height: 720")
 
 
 if __name__ == "__main__":
