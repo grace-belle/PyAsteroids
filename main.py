1604969cef906d1791eed7e18ae3ebf886fe502b
grace-belle <millergb2000@gmail.com>
2026-02-03 11:01:29 -0600

    "lots of updates and improvements to basis of game"

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from logger import log_state
 
 def main():
     pygame.init()
+    
     print("Starting Asteroids with pygame version: VERSION")
     print("Screen width: 1280")
     print("Screen height: 720")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,6 +19,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        time = clock.tick(60) / 1000
+        dt = time
+
 
 
 if __name__ == "__main__":
