ea0e65a40db7e8c1d5109e063060df2a0b8b5209
grace-belle <millergb2000@gmail.com>
2026-02-03 11:04:14 -0600

    missed these files

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -8,16 +12,34 @@ def main():
     print("Starting Asteroids with pygame version: VERSION")
     print("Screen width: 1280")
     print("Screen height: 720")
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = asteroids, updatable, drawable
+    Player.containers = updatable, drawable
+    AsteroidField.containers = updatable
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     dt = 0
+
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         screen.fill("black")
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
         time = clock.tick(60) / 1000
         dt = time
